7636ca6b1b14bb3c665821982720193da243347b
Leastyto <95569132+Leastyto@users.noreply.github.com>
2025-05-31 00:31:52 +1000

    HELP

--- node_2d.gd
+++ node_2d.gd
@@ -1,45 +1,51 @@
 extends Node2D
 
-@onready var mult: Button = $Control/HBoxContainer/Mult
-@onready var mult_back: Button = $Control/HBoxContainer/MultBack
 @onready var auto: Button = $Control/Auto
-@onready var rich_text_label: RichTextLabel = $Control/RichTextLabel
 @onready var timer: Timer = $Control/Timer
+@onready var boba_count: RichTextLabel = $Control/BobaCount
+@onready var rich_text_label: RichTextLabel = $Control/Panel/ScrollContainer/VBoxContainer/HBoxContainer/RichTextLabel
+@onready var viet_boy_buy: RichTextLabel = $Control/Panel/ScrollContainer/VBoxContainer/HBoxContainer/VietBoyBuy
+@onready var viet_man_buy: RichTextLabel = $Control/Panel/ScrollContainer/VBoxContainer/HBoxContainer2/VietManBuy
+@onready var robot_buy: RichTextLabel = $Control/Panel/ScrollContainer/VBoxContainer/HBoxContainer3/RobotBuy
 
-var bobas:int = 0
+var bobas:int = 10000000
 var autoEnable:bool = false
 var multiAmount:int = 1
-func _ready():
-	mult_back.visible = false
+var VietBoyPrice:int = 50
+var VietBoyOwn:int = 0
+var VietManPrice:int = 250
+var VietManOwn:int = 0
+var RobotPrice:int = 1000
+var RobotOwn:int = 0
 
 func _process(_delta):
-	rich_text_label.text = "[center] bobas gathered: " + str(bobas)
-	mult.text = "Multi amount: " + str(multiAmount)
+	boba_count.text = "[center] bobas: " + str(bobas)
+
+func _on_timer_timeout():
+	bobas += (VietBoyOwn)+(VietManOwn*5)+(RobotOwn*19)
 
 func _on_texture_button_pressed():
 	bobas += multiAmount
 
-func _on_mult_pressed():
-	multiAmount += 1
-	UpdateBackButton()
-
-func _on_mult_back_pressed():
-	if multiAmount > 1:
-		multiAmount -= 1
-	UpdateBackButton()
-
-func _on_auto_toggled(toggled_on: bool):
-	if toggled_on:
-		timer.start()
-	else:
-		timer.stop()
+func _on_viet_boy_but_pressed():
+	if bobas >= VietBoyPrice:
+		VietBoyOwn += 1
+		bobas -= VietBoyPrice
+		VietBoyPrice = 50*((1.15)**VietBoyOwn)
+	viet_boy_buy.text = "Vietnamese Boy: " + str(VietBoyOwn) + "\n" + "Price: "+ str(VietBoyPrice) + "\n" + "1 bps"
 
 
-func UpdateBackButton():
-	if multiAmount > 1:
-		mult_back.visible = true
-	elif multiAmount == 1:
-		mult_back.visible = false
+func _on_viet_man_but_pressed():
+	if bobas >= VietManPrice:
+		VietManOwn += 1
+		bobas -= VietManPrice
+		VietManPrice = 250*((1.15)**VietManOwn)
+	viet_man_buy.text = "Vietnamese Man: " + str(VietManOwn) + "\n" + "Price: "+ str(VietManPrice) + "\n" + "5 bps"
 
-func _on_timer_timeout() -> void:
-	bobas += 1
+
+func _on_robot_but_pressed() -> void:
+	if bobas >= RobotPrice:
+		RobotOwn += 1
+		bobas -= RobotPrice
+		RobotPrice = 1000*((1.15)**RobotOwn)
+	robot_buy.text = "Robot: " + str(RobotOwn) + "\n" + "Price: "+ str(RobotPrice) + "\n" + "19 bps"
